46dc1ee73e4555902ce87d82f7502d68298d7c93
Matt Ji <idle911@163.com>
2018-11-01 15:26:30 +0800

    Update and rename test-ctl-mb.py to serial read.py

--- serial read.py
+++ serial read.py
@@ -6,27 +6,16 @@
 1. 停止主程序
 2. python ctl-mb.py ttyS1
 '''
-import serial, json, threading
+import serial 
 import time, sys
-from random import *
-import struct
-import crcmod, json
-#print len(sys.argv)
-'''
-[1,20,10,100]
-'''
+
 if (len(sys.argv) != 2):
     print 'usage: python ctl-rpi.py ttyUSB0'
     exit(1)
 
 path = '/dev/'+sys.argv[1]
 print 'will open ' + path
-def parse_ctl(data):
-    try:
-        pkt = json.loads(data)
-        print pkt
-    except Exception, e:
-        print "this packet is wrong"
+
 
 ser = serial.Serial(path, 115200, timeout=3)
 ser.reset_input_buffer()
@@ -35,8 +24,9 @@ ser.reset_output_buffer()
 x=''
 while True:
     x = ser.readline()
+    #x = ser.read()
     #print "recv: "+":".join("{:02x}".format(ord(c)) for c in x)
         
     if x:
-        parse_ctl(x)
+        print x
         x='' 
